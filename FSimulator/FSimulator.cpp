eabd697a422efa7342101bbf181e58fdfd2cf27f
DIR\MVey <MVey@slb.com>
2017-11-29 10:43:10 +0100

    Refactoring

--- FSimulator/FSimulator.cpp
+++ FSimulator/FSimulator.cpp
@@ -2,12 +2,37 @@
 //
 
 #include "stdafx.h"
+
+// FSimulator.cpp : Defines the entry point for the console application.
+//
+
 #include <memory>
-#include <queue>
+#include <deque>
 #include <iostream>
-#include <vector>
+#include <string>
+#include <limits>
+#include <iterator>
 
 using namespace std;
+
+
+template<class T>
+T pop(deque<T> &queue)
+{
+	T ret = move(queue.front());
+	queue.pop_front();
+	return move(ret);
+}
+
+
+template<class T>
+deque<T> popAll(deque<T> &queue)
+{
+	deque<T> ret;
+	ret.swap(queue);
+	return move(ret);
+}
+
 
 class Ticket
 {
@@ -16,7 +41,9 @@ public:
 	Ticket(Ticket &&) = delete;
 };
 
+
 using TicketEntityPtr = unique_ptr<Ticket>;
+
 
 TicketEntityPtr make_ticket()
 {
@@ -31,14 +58,11 @@ public:
 		: m_speed(speed)
 	{}
 
+	Box(Box &&) = default;
+
 	int numberOfQueuedTickets() const
 	{
 		return m_inboxTickets.size();
-	}
-
-	int numberOfDoneTickets() const
-	{
-		return m_doneTickets.size();
 	}
 
 	int speed() const
@@ -46,119 +70,204 @@ public:
 		return m_speed;
 	}
 
-	void enqueueIncomingTicket(TicketEntityPtr ticket)
+	deque<TicketEntityPtr> &inboxTickets()
 	{
-		m_inboxTickets.push(std::move(ticket));
+		return m_inboxTickets;
 	}
 
+	const deque<TicketEntityPtr> &inboxTickets() const
+	{
+		return m_inboxTickets;
+	}
+
+	deque<TicketEntityPtr> &doneTickets()
+	{
+		return m_doneTickets;
+	}
+
+	const deque<TicketEntityPtr> &doneTickets() const
+	{
+		return m_doneTickets;
+	}
+
+	// Runs for a day
 	void perform()
 	{
 		for (int i = 0; i < m_speed && !m_inboxTickets.empty(); ++i)
-		{
-			TicketEntityPtr ticketBeingProcessed = std::move(m_inboxTickets.front());
-			m_inboxTickets.pop();
-			m_doneTickets.push(std::move(ticketBeingProcessed));
-		}
-	}
-
-	void updateQueues(Box & previousBox)
-	{
-		while (!previousBox.m_doneTickets.empty())
-		{
-			TicketEntityPtr ticketToMove = std::move(previousBox.m_doneTickets.front());
-			previousBox.m_doneTickets.pop();
-			enqueueIncomingTicket(std::move(ticketToMove));
-		}
+			m_doneTickets.push_back(pop(m_inboxTickets));
 	}
 
 private:
 	int m_speed;
-	queue<TicketEntityPtr> m_doneTickets;
-	queue<TicketEntityPtr> m_inboxTickets;
+	deque<TicketEntityPtr> m_doneTickets;
+	deque<TicketEntityPtr> m_inboxTickets;
 };
 
-class Pipeline
+
+class PipelineController
 {
 public:
-	Pipeline(std::vector<Box> boxes)
-		: m_boxes(std::move(boxes))
+	PipelineController(deque<Box> boxes)
+		: m_boxes(move(boxes))
 	{}
 
 	void stepForward()
 	{
+		feedInputBoxQueues();
+
+		// launch boxes sequentially - virtually in parallel in the sim
+		for (auto & box : m_boxes)
+			box.perform();
+
+		// logging/display only
+		printBoxStates();
+
+		// takes last box "done" work to put it in global throughput container
+		updateGlobalThroughput();
+
+		cout << "cumulated throughput: " << m_cumulatedThroughput.size() << endl;
+	}
+
+private:
+	void feedInputBoxQueues()
+	{
+		// transfer each box "done" work to "inputbox" queues of next boxes
+		for (unsigned int i = 1; i < m_boxes.size(); ++i)
+		{
+			deque<TicketEntityPtr> previousDoneTickets = popAll(m_boxes[i - 1].doneTickets());
+			move(previousDoneTickets.begin(), previousDoneTickets.end(), back_inserter(m_boxes[i].inboxTickets()));
+		}
+
+		if (m_boxes.empty())
+			return;
+
+		// feed new work in first box
 		auto & firstBox = m_boxes.front();
 		for (int i = 0; i < firstBox.speed(); ++i) {
-			firstBox.enqueueIncomingTicket(make_ticket());
+			firstBox.inboxTickets().push_back(make_ticket());
 		}
+	}
 
-		for (auto & box : m_boxes)
-		{
-			box.perform();
-		}
+	// clear work from last box to update global throughput
+	void updateGlobalThroughput()
+	{
+		if (m_boxes.empty())
+			return;
 
+		deque<TicketEntityPtr> outputtedTickets = popAll(m_boxes.back().doneTickets());
+		move(outputtedTickets.begin(), outputtedTickets.end(), back_inserter(m_cumulatedThroughput));
+	}
+
+	void printBoxStates() const
+	{
 		cout << "system state:" << endl;
-		
+
 		int i = 0;
 		for (const auto & box : m_boxes)
 		{
-			cout << "box " << i << " (speed: " << box.speed() << " )" << endl;
+			cout << "box " << i << " (speed: " << box.speed() << ")" << endl;
 			cout << "\t queue: " << box.numberOfQueuedTickets() << endl;
-			cout << "\t in progress: " << box.numberOfDoneTickets() << endl << endl;		
+			cout << "\t in progress: " << box.doneTickets().size() << endl << endl;
 			++i;
 		}
+	}
 
-		int throughPut = 0;
-		cout << "Throughput: " << throughPut << endl;
+	deque<Box> m_boxes;
+	deque<TicketEntityPtr> m_cumulatedThroughput;
+	int m_lastDayThroughput;
+};
 
-		// end of the day
-		for (int i = 1; i < m_boxes.size(); ++i)
+
+template<class input_stream>
+void getInputLine(const input_stream & istream, string & output)
+{
+	getline(cin, output);
+}
+
+
+int waitForPositiveIntegerFromStdIn()
+{
+	int ret = -1;
+	while (ret < 0 || cin.fail()) {
+		if (cin.fail()) {
+			cout << "Please enter an positive integer" << endl;
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		}
+		cin >> ret;
+	}
+	return ret;
+}
+
+
+constexpr unsigned int str2int(const char* str, int h = 0)
+{
+	return !str[h] ? 5381 : (str2int(str, h + 1) * 33) ^ str[h];
+}
+
+class CommandLoop
+{
+public:
+	CommandLoop(PipelineController &pipelineController)
+		: m_pipelineController(pipelineController)
+	{}
+
+	void exec()
+	{
+		string ans = "y";
+
+		while (ans != "q") {
+
+			switch (str2int(ans.c_str()))
+			{
+			case str2int("q"):
+				break;
+			default:
+				runCommand(ans);
+				cout << "What do you want to do? (q: quit, anything else: continue)" << endl;
+			}
+
+			getInputLine(cin, ans);
+		}
+
+	}
+
+	void runCommand(const string & command)
+	{
+		switch (str2int(command.c_str()))
 		{
-			m_boxes[i].updateQueues(m_boxes[i - 1]);
+		default:
+			m_pipelineController.stepForward();
 		}
 	}
 
 private:
-	std::vector<Box> m_boxes;
+	PipelineController & m_pipelineController;
+
 };
 
 
 int main()
 {
 	cout << "Number of boxes? " << endl;
-	int nbBoxes = 0;
-	cin >> nbBoxes;
+	int nbBoxes = waitForPositiveIntegerFromStdIn();
 
 
-	vector<Box> boxQueue;
+	deque<Box> boxQueue;
 	for (int i = 0; i < nbBoxes; ++i)
 	{
-		cout << "Speed for box " << i <<" ? " << endl;
-		int speed = 0;
-		cin >> speed;
+		cout << "Speed for box " << i << "? " << endl;
+		int speed = waitForPositiveIntegerFromStdIn();
 		boxQueue.push_back(Box(speed));
 	}
 
-	Pipeline p(std::move(boxQueue));
+	cin.ignore(numeric_limits<streamsize>::max(), '\n'); //flush stdin buffer
 
-	p.stepForward();
+	PipelineController pipelineController(move(boxQueue));
+	CommandLoop commandLoop(pipelineController);
 
-	while(true) {
-		cout << "Do you want to continue (enter 0 for no or 1 for yes) ?" << endl;
-		int ans = -1;
-		cin >> ans;
-
-		if (ans == 0)
-		{
-			return 0;
-		}
-		else
-		{
-			p.stepForward();
-		}
-	}
-	
+	commandLoop.exec();
 
 
-    return 0;
+	return 0;
 }
-
